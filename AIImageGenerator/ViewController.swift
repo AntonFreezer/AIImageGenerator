c39117824ef2b9ca8ebb4885f301de9923f23313
AntonFreezer <anton.kholodkov@gmail.com>
2024-03-05 17:40:09 +0300

    Initial Commit

---
+++ AIImageGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AIImageGenerator
+//
+//  Created by Anton Kholodkov on 05.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
