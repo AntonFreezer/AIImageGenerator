2705eb600c231001bea65d5bf8ce1201fbecb00d
AntonFreezer <anton.kholodkov@gmail.com>
2024-03-05 19:14:53 +0300

    expand GenericViewController

--- AIImageGenerator/Helper/Shared/Base/GenericViewController.swift
+++ AIImageGenerator/Helper/Shared/Base/GenericViewController.swift
@@ -21,3 +21,48 @@ class GenericViewController<T: UIView>: UIViewController {
     }
     
 }
+
+//MARK: - Loadable
+extension GenericViewController: Loadable {
+    func showLoading() {
+        loadingView.spinner.startAnimating()
+        rootView.addSubview(loadingView)
+        loadingView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        loadingView.backgroundColor = view.backgroundColor?.withAlphaComponent(0.85)
+    }
+    
+    func hideLoading() {
+        loadingView.spinner.stopAnimating()
+        loadingView.removeFromSuperview()
+    }
+}
+
+//MARK: - ErrorPresentable
+extension GenericViewController: ErrorPresentable {
+    func showError(
+        _ title: String,
+        message: String?,
+        actionTitle: String?,
+        action: ((UIView) -> Void)?
+    ) {
+        errorView.title = title
+        errorView.message = message
+        errorView.actionTitle = actionTitle
+        errorView.action = action
+        
+        rootView.addSubview(errorView)
+        errorView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(16)
+        }
+        
+        errorView.backgroundColor = view.backgroundColor
+    }
+    
+    func hideError() {
+        errorView.removeFromSuperview()
+    }
+}
+
